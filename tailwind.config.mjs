0d267f6786b69dd0cfd71bfc72ea69110e2365e2
daridev <darideveloper@gmail.com>
2025-04-23 19:11:31 -0600

    feat: setup tailwind

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  colors: {
+    brand: {
+      blue: '#4f6a9e',
+      white: '#ffffff',
+      black: '#000000',
+      gold: '#c7b66b',
+    },
+  }
+};
