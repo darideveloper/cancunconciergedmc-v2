14ee83d54dcbc39020256762ad8a5bea490a8832
daridev <darideveloper@gmail.com>
2025-05-06 13:58:01 -0600

    feat (Footer): layout and responsive

--- src/data/nav.ts
+++ src/data/nav.ts
@@ -1,6 +1,22 @@
-export const navLinks = [
-    {
-        key: 'about',
-        url: '/about'
-    },
+export const navLinks: { text: string; url: string }[] = [
+  {
+    text: 'home',
+    url: '/',
+  },
+  {
+    text: 'about',
+    url: '/about',
+  },
+  {
+    text: 'testimonials',
+    url: '/testimonials',
+  },
+  {
+    text: 'services',
+    url: '/services',
+  },
+  {
+    text: 'transportation',
+    url: '/transportation',
+  },
 ]
---
+++ src/data/social.ts
@@ -0,0 +1,22 @@
+export const socialLinks: { text: string; url: string }[] = [
+  {
+    text: '998 23 91 978',
+    url: 'tel:+529982391978',
+  },
+  {
+    text: 'email',
+    url: 'mailto:info@cancunconciergedmc.com',
+  },
+  {
+    text: 'google maps',
+    url: 'https://goo.gl/maps/2v1x5Z3f6z7g4j8y7',
+  },
+  {
+    text: 'Facebook',
+    url: 'https://www.facebook.com/cancunconciergemx',
+  },
+  {
+    text: 'Instagram',
+    url: 'https://www.instagram.com/cancunconcierge/',
+  },
+]
