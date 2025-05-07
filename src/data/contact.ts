a2e7454fcb231880b1e3854e712a2ed049c98cb8
daridev <darideveloper@gmail.com>
2025-05-06 19:08:22 -0600

    feat (Footer): split links in socialLinks and contactClassicLinks

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -1,13 +1,5 @@
 export const socialLinks: { text: string; url: string }[] = [
   {
-    text: '998 23 91 978',
-    url: 'tel:+529982391978',
-  },
-  {
-    text: 'email',
-    url: 'mailto:info@cancunconciergedmc.com',
-  },
-  {
     text: 'google maps',
     url: 'https://goo.gl/maps/2v1x5Z3f6z7g4j8y7',
   },
@@ -19,4 +11,16 @@ export const socialLinks: { text: string; url: string }[] = [
     text: 'Instagram',
     url: 'https://www.instagram.com/cancunconcierge/',
   },
+]
+
+
+export const contactClassicLinks: { text: string; url: string }[] = [
+  {
+    text: '+52 998 23 91 978',
+    url: 'tel:+529982391978',
+  },
+  {
+    text: 'info@cancunconciergedmc.com',
+    url: 'mailto:info@cancunconciergedmc.com',
+  }
 ]
