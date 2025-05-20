02ffe9716456bad874cf8a7887e7970b304f81db
daridev <darideveloper@gmail.com>
2025-05-19 19:23:20 -0600

    fix (seo): use robots.txt in public folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,21 @@ import react from '@astrojs/react'
 
 import showTailwindcssBreakpoint from 'astro-show-tailwindcss-breakpoint'
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
   site: 'https://cancunconciergedmc.com',
-  integrations: [react(), showTailwindcssBreakpoint(), sitemap()],
+  integrations: [
+    react(),
+    showTailwindcssBreakpoint(),
+    sitemap({
+      i18n: {
+        defaultLocale: 'en',
+        locales: ['en', 'es'],
+      },
+      robotsTxt: false,
+    })
+  ],
   vite: { plugins: [tailwindcss()] },
   i18n: {
     locales: ["es", "en"],
