e0d6a574bb38fe39e46e936a365b624a05f8338b
daridev <darideveloper@gmail.com>
2025-05-19 18:50:52 -0600

    feat (seo): create sitemap and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,19 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
+import tailwindcss from '@tailwindcss/vite'
 
-import react from '@astrojs/react';
+import react from '@astrojs/react'
 
-import showTailwindcssBreakpoint from 'astro-show-tailwindcss-breakpoint';
+import showTailwindcssBreakpoint from 'astro-show-tailwindcss-breakpoint'
+
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [starlight({
-    title: 'Docs with Tailwind',
-    customCss: [
-      // Path to your Tailwind base styles:
-      './src/styles/global.css',
-    ],
-  }), react(), showTailwindcssBreakpoint()],
+  site: 'https://cancunconciergedmc.com',
+  integrations: [react(), showTailwindcssBreakpoint(), sitemap()],
   vite: { plugins: [tailwindcss()] },
   i18n: {
     locales: ["es", "en"],
     defaultLocale: "en",
   }
-});
+})
