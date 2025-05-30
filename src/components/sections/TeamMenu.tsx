9bd6be2d0a08272df6180aff96a972099d786865
daridev <darideveloper@gmail.com>
2025-05-29 19:45:24 -0600

    chore (Team): update bios, render md and add section description

--- src/components/sections/TeamMenu.tsx
+++ src/components/sections/TeamMenu.tsx
@@ -4,6 +4,7 @@ import TeamCard from '../ui/TeamCard'
 // Libs
 import { clsx } from 'clsx'
 import { useTranslations } from '../../i18n/utils'
+import { marked } from 'marked'
 
 // Hooks
 import { useState } from 'react'
@@ -11,8 +12,6 @@ import { useState } from 'react'
 // Styles
 import { titleClasses, getMenuLinksClasses } from '../../styles/classes'
 
-// Animation Library
-import { motion } from 'motion/react'
 
 // Props interface
 interface TeamProps {
@@ -30,57 +29,56 @@ export default function Team({ lang }: TeamProps) {
 
   return (
     <section
-      className={clsx(
-        'container',
-        'team',
-        'flex',
-        'flex-col md:flex-row',
-        'gap-8'
-      )}
+      className={clsx('container')}
       id='team'
     >
-      <div className={clsx('aside', 'w-full md:w-4/12 lg:w-1/4')}>
-        {/* Page title */}
-        <h2 className={clsx(titleClasses)}>Our Team</h2>
+      {/* Page title */}
+      <h2 className={clsx(titleClasses)}>{t('teamMenu.title')}</h2>
 
-        {/* Links */}
-        <ul>
-          {teamKeys.map((teamKey) => (
-            <li key={teamKey}>
-              <button
-                onClick={() => {
-                  setActiveOption(teamKey)
-                }}
-                className={getMenuLinksClasses(activeOption == teamKey)}
-              >
-                {teamData[teamKey].name}
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
+      {/* Text */}
+      <div dangerouslySetInnerHTML={{ __html: marked(t('teamMenu.text')) }} />
 
-      <div className={clsx('content', 'w-full md:w-7/12 lg:w-3/4')}>
-        {teamKeys.map((teamKey) => {
-          const member = teamData[teamKey]
-          return (
-            activeOption === teamKey && (
-              <motion.div
-                key={teamKey}
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: -20 }}
-                transition={{ duration: 0.5 }}
-              >
+      <div
+        className={clsx(
+          'team-data',
+          'flex',
+          'flex-col md:flex-row',
+          'gap-8',
+          'mt-12'
+        )}
+      >
+        <div className={clsx('aside', 'w-full md:w-4/12 lg:w-1/4')}>
+          {/* Links */}
+          <ul>
+            {teamKeys.map((teamKey) => (
+              <li key={teamKey}>
+                <button
+                  onClick={() => {
+                    setActiveOption(teamKey)
+                  }}
+                  className={getMenuLinksClasses(activeOption == teamKey)}
+                >
+                  {teamData[teamKey].name}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className={clsx('content', 'w-full md:w-7/12 lg:w-3/4')}>
+          {teamKeys.map((teamKey) => {
+            const member = teamData[teamKey]
+            return (
+              activeOption === teamKey && (
                 <TeamCard
                   id={teamKey}
                   name={member.name}
                   bio={member.bio}
                 />
-              </motion.div>
+              )
             )
-          )
-        })}
+          })}
+        </div>
       </div>
     </section>
   )
--- src/components/ui/TeamCard.tsx
+++ src/components/ui/TeamCard.tsx
@@ -3,6 +3,7 @@ import { clsx } from 'clsx'
 
 // Styles
 import { titleClasses } from '../../styles/classes'
+import { marked } from 'marked'
 
 // Props interface
 interface Props {
@@ -13,20 +14,14 @@ interface Props {
 }
 
 export default function TeamCard({ id, name, bio, className = "" }: Props) {
-  // Split the bio into multiple texts
-  const texts = bio.split(';')
 
   return (
     <article className={clsx('team-card', className)}>
       <h3 className={clsx('card-title', titleClasses, '!text-xl')}>{name}</h3>
-      {texts.map((text, index) => (
-        <p
-          key={index}
-          className='bio-text'
-        >
-          {text}
-        </p>
-      ))}
+      <div
+        className='bio-text md-content'
+        dangerouslySetInnerHTML={{ __html: marked(bio) }}
+      />
       <img
         src={`/team/${id}.webp`}
         alt={`${name}'s photo`}
