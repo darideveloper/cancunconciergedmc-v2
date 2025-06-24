65c6588b14e473986595b494da05bc88692c5363
daridev <darideveloper@gmail.com>
2025-06-23 18:01:25 -0600

    feat (gallery): render multiple sliders

--- src/components/sections/base/GallerySwipper.tsx
+++ src/components/sections/base/GallerySwipper.tsx
@@ -1,36 +1,42 @@
-import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Autoplay } from 'swiper/modules';
-import clsx from 'clsx';
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Autoplay } from 'swiper/modules'
+import clsx from 'clsx'
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
+import 'swiper/css'
+import 'swiper/css/navigation'
 
+// Props
 interface GallerySwipperProps {
-  title: string;
-  images: string[];
-  className?: string;
+  title: string
+  images: string[]
+  className?: string
 }
 
-export default function GallerySwipper({ title, images, className }: GallerySwipperProps) {
+export default function GallerySwipper({
+  title,
+  images,
+  className,
+}: GallerySwipperProps) {
   return (
     <section
-      id="gallery"
+      id='gallery'
       className={clsx('container', 'w-full', 'px-0', '!my-12', className)}
     >
       {/* Title */}
-      <h2 className={clsx(
-        'text-3xl',
-        'font-bold',
-        'text-black',
-        'font-title',
-        'tracking-wide',
-        'leading-tight',
-        'my-6',
-        'text-center',
-        'mb-8'
-      )}>
+      <h2
+        className={clsx(
+          'text-3xl',
+          'font-bold',
+          'text-black',
+          'font-title',
+          'tracking-wide',
+          'leading-tight',
+          'my-6',
+          'text-center',
+          'mb-8'
+        )}
+      >
         {title}
       </h2>
 
@@ -62,25 +68,37 @@ export default function GallerySwipper({ title, images, className }: GallerySwip
           loop={true}
           className={clsx('gallery-swiper')}
         >
-          {images.map((image, index) => (
-            <SwiperSlide key={index}>
-              <div className={clsx(
-                'relative',
-                'aspect-[16/9]',
-                'overflow-hidden',
-                'rounded-lg'
-              )}>
-                <img
-                  src={image.replace('/public', '')}
-                  alt={`Gallery image ${index + 1}`}
-                  className={clsx('w-full', 'h-full', 'object-cover', 'select-none')}
-                  loading="lazy"
-                />
-              </div>
-            </SwiperSlide>
-          ))}
+          {images.map((image, index) => {
+            const imageSrc = image.replace('/public', '')
+            const imageAlt = `Gallery image ${imageSrc.split('/').pop()?.split('.')[0].replaceAll(/-/g, ' ').replaceAll('%20', ' ')}`
+
+            return (
+              <SwiperSlide key={index}>
+                <div
+                  className={clsx(
+                    'relative',
+                    'aspect-[16/9]',
+                    'overflow-hidden',
+                    'rounded-lg'
+                  )}
+                >
+                  <img
+                    src={imageSrc}
+                    alt={imageAlt}
+                    className={clsx(
+                      'w-full',
+                      'h-full',
+                      'object-cover',
+                      'select-none'
+                    )}
+                    loading='lazy'
+                  />
+                </div>
+              </SwiperSlide>
+            )
+          })}
         </Swiper>
       </div>
     </section>
-  );
-} 
+  )
+}
