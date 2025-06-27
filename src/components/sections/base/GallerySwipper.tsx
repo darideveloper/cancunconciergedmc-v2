3f691901c9143206d7ae230ce768a50ce7c45733
daridev <darideveloper@gmail.com>
2025-06-27 16:59:08 -0600

    fix (/gallery): load images in build mode

--- src/components/sections/base/GallerySwipper.tsx
+++ src/components/sections/base/GallerySwipper.tsx
@@ -70,7 +70,12 @@ export default function GallerySwipper({
         >
           {images.map((image, index) => {
             const imageSrc = image.replace('/public', '')
-            const imageAlt = `${imageSrc.split('/').pop()?.split('.')[0].replaceAll(/-/g, ' ').replaceAll('%20', ' ')}`
+            const imageAlt = `${imageSrc
+              .split('/')
+              .pop()
+              ?.split('.')[0]
+              .replaceAll(/-/g, ' ')
+              .replaceAll('%20', ' ')}`
 
             return (
               <SwiperSlide key={index}>
