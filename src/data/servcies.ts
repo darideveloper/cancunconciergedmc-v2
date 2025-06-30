9b43f5468cbbe807880a8b3fb9276750227cb5b9
daridev <darideveloper@gmail.com>
2025-06-30 12:30:19 -0600

    feat (Services Menu): get keys and icons from data

--- src/data/servcies.ts
+++ src/data/servcies.ts
@@ -1,9 +1,20 @@
-export const servicesItems = [
-    'transfers',
-    'yacht',
-    'golf',
-    'teambuilding',
-    'desk',
-    'entertainment',
-    'photography',
-]
+// Icons
+import {
+  MdAirplanemodeActive,
+  MdDirectionsBoat,
+  MdGolfCourse,
+  MdGroups2,
+  MdLocalHospital,
+  MdOutlineStarPurple500,
+  MdCameraAlt,
+} from 'react-icons/md'
+
+export const servicesItemsIcons = {
+  'transfers': MdAirplanemodeActive,
+  'yacht': MdDirectionsBoat,
+  'golf': MdGolfCourse,
+  'teambuilding': MdGroups2,
+  'desk': MdLocalHospital,
+  'entertainment': MdOutlineStarPurple500,
+  'photography': MdCameraAlt,
+}
