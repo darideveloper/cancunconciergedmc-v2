5af4ab4c7d9e57b252367645d50c18711b4ebfb5
daridev <darideveloper@gmail.com>
2025-07-21 16:30:31 -0600

    feat (SEO): optimize dynamic data

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -1,6 +1,29 @@
 export const phoneNumber = '5219982391978'
 export const whatsappBaseUrl = `https://api.whatsapp.com/send?phone=${phoneNumber}`
 
+// SEO Contact Data
+export const phoneUnformatted = '+52 998 23 91 978'
+export const email = 'info@cancunconciergedmc.com'
+export const addressElems = {
+  addressLocality: 'Cancun',
+  addressRegion: 'Quintana Roo',
+  addressCountry: 'Mexico',
+}
+export const socialNetworks = [
+  {
+    name: 'Facebook',
+    link: 'https://www.facebook.com/cancunconciergemx',
+  },
+  {
+    name: 'Instagram',
+    link: 'https://www.instagram.com/cancunconcierge/',
+  },
+  {
+    name: 'Google Maps',
+    link: 'https://www.google.com.mx/maps/place/Canc%C3%BAn,+Q.R./@21.1214334,-87.1682536,11z/data=!4m6!3m5!1s0x8f4c2b05aef653db:0xce32b73c625fcd8a!8m2!3d21.161908!4d-86.8515279!16zL20vMDFxOTht?entry=tts&g_ep=EgoyMDI1MDUwNS4wIPu8ASoASAFQAw%3D%3D&skid=1ed7bde6-ff37-492f-90a0-2add32d28ada',
+  },
+]
+
 export const socialLinks: { text: string; url: string }[] = [
   {
     text: 'google maps',
@@ -15,7 +38,6 @@ export const socialLinks: { text: string; url: string }[] = [
     url: 'https://www.instagram.com/cancunconcierge/',
   },
 ]
-
 
 export const contactClassicLinks: { text: string; url: string }[] = [
   {
